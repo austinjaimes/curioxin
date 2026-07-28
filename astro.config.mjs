cfa2b7e55d4f194d5b4441f92568240f380e3e95
Austin Jaimes <sauracompany@gmail.com>
2026-07-28 10:40:30 -0600

    Arreglando netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://curioxin.com",
+
+  output: "server",
+
+  adapter: netlify(),
+
   integrations: [
     sitemap()
   ],
+
   vite: {
     plugins: [tailwindcss()]
   }
